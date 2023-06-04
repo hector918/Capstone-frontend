0c8b037d9cb2cd7a02249a5eee8ecd818cd4c4c4
bigdaddyjean <jeannappoleon@pursuit.org>
2023-06-04 09:56:21 -0400

    reading assistance

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,52 @@
+import { Container } from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function navbar() {
+  return (
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+      </Helmet>
+      <Navbar fluid="true" bg="dark" variant="dark" expand="lg" sticky="top">
+        <Container fluid>
+          <Navbar.Brand
+            href="/"
+            className="navbar-brand mb-0 h1 mx-auto pl-4"
+            style={{ fontSize: "2rem" }}
+          >
+            <img
+              className="d-inline-block "
+              src={process.env.PUBLIC_URL + "/caplogo.png"}
+              width="100"
+              height="100"
+              alt=""
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav className="ml-auto h2">
+              <Link to="/library" className="nav-link pill-link">
+                Library
+              </Link>
+              <Link to="/login" className="nav-link pill-link">
+                Your Account
+              </Link>
+              <Link to="/about" className="nav-link pill-link">
+                About The Developers
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default navbar;
